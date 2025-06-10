23abd0cc4cb70429fc5148386976f57c92bf7b87
Steven Pierre <96037516+kiss42@users.noreply.github.com>
2025-06-10 17:43:18 -0400

    update to icon

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './styles/globals.css'
 import AppRouter from './router.jsx'
 import { BookingProvider } from './context/BookingContext'
+import lotusIcon from './assets/lotuslogo.png'
+
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/png'
+link.href = lotusIcon
+document.head.appendChild(link)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
